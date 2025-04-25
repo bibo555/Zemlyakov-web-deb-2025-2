f060309ff353fb1dbc1a93ae9e56ee9c52d440cf
bibo555 <145836651+bibo555@users.noreply.github.com>
2025-04-25 15:37:33 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from lab1_1.app.app import app as lab1_app
 from lab2_2.app2.app import app as lab2_app
 from lab3.app import app as lab3_app
-from app import app as root_app
+from root_app.app import app as root_app
 
 app = DispatcherMiddleware(root_app, {
     '/lab1': lab1_app,
