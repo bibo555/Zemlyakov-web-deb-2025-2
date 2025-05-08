199554209d68df833dc777952ec8540fd76ac727
bibo555 <artemz0597@mail.ru>
2025-05-08 18:23:41 +0300

    sync

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from lab1_1.app.app import app as lab1_app
 from lab2_2.app2.app import app as lab2_app
 from lab3.app import app as lab3_app
+from lab4.app import app as lab4_app
 from app import app as root_app
 
 app = DispatcherMiddleware(root_app, {
     '/lab1': lab1_app,
     '/lab2': lab2_app,
     '/lab3': lab3_app
+    '/lab4': lab4_app
 })
 application = app
